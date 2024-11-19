4662c1c498ae473528978846c3e4d8e29abac484
itayz1e <itayz1e@gmail.com>
2024-11-19 21:22:24 +0200

    wip - handle search filter with id

--- Rick and Morty/src/components/SearchBar.tsx
+++ Rick and Morty/src/components/SearchBar.tsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 import "../style/SearchBar.scss";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const SearchBar = () => {
+  const [searchId, setSearchId] = useState<string>("");
+
+  const { data: character, refetch } = useQuery({
+    queryKey: ["character", searchId],
+    queryFn: async () => {
+      if (!searchId) return null;
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character/${searchId}`
+      );
+      return response.data;
+    },
+    enabled: false,
+  });
+
+  const handleSearch = async () => {
+    if (searchId.trim()) {
+      const result = await refetch();
+      console.log("Refetch Result:", result);
+      console.log();
+      setSearchId("");
+    } else {
+      alert("Please enter a character ID.");
+    }
+  };
+
   return (
     <div className="main_search_container">
       <div className="search_container">
@@ -9,10 +37,14 @@ const SearchBar = () => {
           type="text"
           className="search_input"
           placeholder="Browse for characters..."
+          value={searchId}
+          onChange={(e) => setSearchId(e.target.value)}
         />
-      <div>
-        <button className="search_button">Go</button>
-      </div>
+        <div>
+          <button onClick={handleSearch} className="search_button">
+            Go
+          </button>
+        </div>
       </div>
     </div>
   );
