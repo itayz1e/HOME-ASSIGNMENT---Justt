15f29eb3d6181b022d9e497470999870c6942021
itayz1e <itayz1e@gmail.com>
2024-11-21 20:36:30 +0200

    displaying information from context in CharacterCard - one issue - does not show multiple results, only one result

--- Rick and Morty/src/components/CharacterCard.tsx
+++ Rick and Morty/src/components/CharacterCard.tsx
@@ -1,44 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import BigRow from "./BigRow";
 import ArrowIcon from "../assets/ArrowIcon";
 import TableBar from "./TableBar";
 import noSearch from "../assets/Group 204.png";
+import noSearchText from "../assets/Search for a character i.d in order to view a character.png";
+import { useCharacterContext } from "../hooks/CharacterContext";
 
 const CharacterCard = () => {
+  const { characterData } = useCharacterContext();
+  const [expandedCharacters, setExpandedCharacters] = useState<number[]>([]);
 
+  const toggleExpand = (id: number) => {
+    setExpandedCharacters((prev) =>
+      prev.includes(id) ? prev.filter((charId) => charId !== id) : [...prev, id]
+    );
+  };
 
+  if (!characterData) {
+    return(
+
+      <td colSpan={8} className="no-results">
+    <div className="noSearch">
+      <img src={noSearch} alt="No results" />
+      <h1>Search for a character</h1>
+      </div>
+  </td>
+    );
+  }
 
   return (
     <div>
       <div className="table">
-        <TableBar
-          tbody={
-            <>
-              <tr>
-                {/* <td>{characterData.name}</td> */}
-                <td>NAME</td>
-                <td>species</td>
-                <td>status</td>
-                <td>origin</td>
-                <td>gender</td>
-                <td>
-                  <button>
-                    <ArrowIcon />
-                  </button>
-                </td>
-              </tr>
-              <tr className="expanded-row">
-                {/* <td colSpan={7}>
-                     <BigRow character={character} />
-                   </td> */}
-              </tr>
-            </>
-          }
-        />
-        <div>
-          <img src={noSearch} alt="No results" />
-          <h1>Search for a character</h1>
-        </div>
+        
+          <TableBar
+            tbody={
+              <>
+                <tr>
+                  <td>{characterData?.id}</td>
+                  <td>{characterData?.name}</td>
+                  <td>{characterData?.species}</td>
+                  <td>{characterData?.status}</td>
+                  <td>{characterData?.origin.name}</td>
+                  <td>{characterData?.gender}</td>
+                  <td>
+                  <button onClick={() => toggleExpand(characterData.id)}>
+                        <ArrowIcon />
+                      </button>
+                  </td>
+                </tr>
+                {expandedCharacters.includes(characterData.id) && (
+                    <tr className="expanded-row">
+                      <td colSpan={7}>
+                        <BigRow character={characterData} />
+                      </td>
+                    </tr>
+                  )}
+              </>
+            }
+          />
       </div>
     </div>
   );
--- Rick and Morty/src/hooks/CharacterContext.tsx
+++ Rick and Morty/src/hooks/CharacterContext.tsx
@@ -1,14 +1,27 @@
 import React, { createContext, ReactNode, useContext, useState } from "react";
 
+
+
 export interface Character {
   id: number;
   name: string;
+  status: "Alive" | "Dead" | "unknown";
   species: string;
-  status: string;
-  origin: { name: string };
-  gender: string;
+  type: string;
+  gender: "Female" | "Male" | "Genderless" | "unknown";
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
   image: string;
-}
+  episode: string[];
+  url: string;
+  created: string;
+};
 
 
 export interface CharacterContextType {
