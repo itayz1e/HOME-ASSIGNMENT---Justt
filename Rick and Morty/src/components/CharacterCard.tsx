a95c5488f25fd32c324411ef22f4386b67a43658
itayz1e <itayz1e@gmail.com>
2024-11-25 12:21:58 +0200

    adding a function to the pagination buttons and displaying them

--- Rick and Morty/src/components/CharacterCard.tsx
+++ Rick and Morty/src/components/CharacterCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import BigRow from "./BigRow";
-import ArrowIcon from "../assets/ArrowIcon";
+import ArrowIcon from "../assets/DownArrowIcon";
 import TableBar from "./TableBar";
 import noSearch from "../assets/images/Group 204.png";
 import { useCharacterContext } from "../hooks/CharacterContext";
