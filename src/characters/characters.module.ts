26137e20a9f320ed766cc74c0f06fea705512689
itayz1e <itayz1e@gmail.com>
2024-12-01 23:24:31 +0200

    Initial commit

--- src/characters/characters.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CharactersController } from './characters.controller';
-
-@Module({
-  controllers: [CharactersController]
-})
-export class CharactersModule {}
